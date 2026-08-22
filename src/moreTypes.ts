77b9f75bb94d505453fc89143a87e0d3882d1700
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-23 00:09:35 +0530

    add comments for better understanding

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -7,6 +7,6 @@ type Book = {
 
 let bookString = '{"name": "who moved my cheese"}';
 
-let bookObject = JSON.parse(bookString) as Book;
+let bookObject = JSON.parse(bookString) as Book; // if we do not use as Book, typescript will infer the type of bookObject to be any and we will not get any type checking. By using as Book, we are telling typescript that we know the type of the variable `bookObject` is Book and we are asserting it to be of type Book. This is useful when we are sure about the type of a variable but typescript is not able to infer it correctly.
 
 console.log(bookObject.name);
