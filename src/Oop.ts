31a8546d1d71cc85bfdbe0aa820cb8890bc6f34a
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-24 19:42:04 +0530

    add class Chai with properties and constructor

---
+++ src/Oop.ts
@@ -0,0 +1,12 @@
+// class Chai{
+//     flavour : string;
+//     price : Number;
+
+//     constructor(flavour : string, price : Number){
+//         this.flavour = flavour
+//         this.price = price
+//     }
+// }
+
+// const masalaChai = new Chai("Ginger",20);
+// masalaChai.flavour = "masala"
