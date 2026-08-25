4b5109343499436b2cfa43cdfe58d9b47fec0c6a
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-26 01:01:09 +0530

    add interfaces for Chai and shop with example implementations

---
+++ src/Interface&Generics.ts
@@ -0,0 +1,18 @@
+// interface define the structure of object but do not genrate the javascript code
+
+interface Chai {
+    flavor : string
+    price : number
+    milk? : boolean
+}
+
+const masala : Chai = {
+    flavor : "masala",
+    price : 30
+}
+
+interface shop{
+    readonly id : number
+    name : string
+}
+const s : shop = {id : 1, name : "meena caffe"}
