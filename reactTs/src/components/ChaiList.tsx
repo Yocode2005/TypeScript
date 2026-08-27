b30e5f0c3e04a457f6bb995f9a170cc7a8eac28b
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-27 20:07:37 +0530

    refactor ChaiList component to accept items as props and render ChaiCard components

--- reactTs/src/components/ChaiList.tsx
+++ reactTs/src/components/ChaiList.tsx
@@ -1,6 +1,22 @@
-export function ChaiList(){
+import type { Chai } from "../types"
+import { ChaiCard } from "./ChaiCard"
+
+interface ChaiListProps{
+    items : Chai[]
+}
+
+export function ChaiList({items} : ChaiListProps){
     return(
-        <div>ChaiList</div>
+        <div>
+            {items.map((chai) => (
+                <ChaiCard
+                 key={chai.id}
+                   name={chai.name}
+                   price={chai.price} 
+                   isSpecial={chai.price > 30}
+                 />
+            ))}
+        </div>
     )
 }
 
