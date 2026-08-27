882138612e28cda25d7715a1a0af5188712e26c4
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-27 10:46:08 +0530

    clear the project

--- reactTs/src/App.tsx
+++ reactTs/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { ChaiCard } from './components/ChaiCard.tsx'
 
 function App() {
   return (
     <>
       <div>
         <h1>Vite + React</h1>
+        <ChaiCard 
+        name="Headphones"
+        price="5000"
+        />
       </div>
     </>
   )
---
+++ reactTs/src/components/ChaiCard.tsx
@@ -0,0 +1,9 @@
+export function ChaiCard({name, price, isSpecial = false}){
+    return (
+        <article>
+            <h2>
+                {name} {isSpecial && <span>⭐</span>}
+            </h2>
+        </article>
+    )
+}
